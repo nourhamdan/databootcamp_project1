e25918e417840b48ca8e00f90aa57c7ddbb40571
FabulousErin <77863014+FabulousErin@users.noreply.github.com>
2021-03-16 19:40:57 -0500

    Add files via upload

---
+++ erin_analysis.py
@@ -0,0 +1,8 @@
+# Add and commit your `hello.py`.
+git add hello.py
+git commit -m "First commit."
+
+# Create and checkout a new branch, called `helpers`.
+git branch helpers
+git checkout helpers
+# Or: git checkout --branch helpers
